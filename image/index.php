f4aab3569a8e93e4469c11cd3c367d64dd62aad0
Grozdimir Kutsarov <gkutsarov123@gmail.com>
2024-11-06 04:05:15 -0500

    Initial Commit

---
+++ image/index.php
@@ -0,0 +1,30 @@
+<?php
+// Get environment variables from Kubernetes
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Simple query to fetch data
+$sql = "SELECT id, name FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
